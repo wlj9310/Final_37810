94b7651198690469cc3fbdeba9bcc1738400f5e9
lijing <lijing@uchicago.edu>
2015-11-02 01:14:06 +0800

    Gibbs-

--- Gibbs_.R
+++ Gibbs_.R
@@ -1,13 +1,18 @@
-Gibbs_<-function(startvalue,iterations,burnIn,B)
+Gibbs_<-function(x0,y0,iterations,B,burnIn=0)
 {
-  x<-c(startvalue[1],rep(NA,iterations-1))
-  y<-c(startvalue[2],rep(NA,iterations-1))
-  for(i in 1:(iterations-1))
+  if(x0>0&&x0<B&&y0>0&&y0<B)
   {
-    x[i+1]<-(-log(1-runif(1)*(1-exp(-y[i]*B)))/y[i])
-    y[i+1]<-(-log(1-runif(1)*(1-exp(-x[i+1]*B)))/x[i+1])
+    x<-c(x0,rep(NA,iterations-1))
+    y<-c(y0,rep(NA,iterations-1))
+    for(i in 1:(iterations-1))
+    {
+      x[i+1]<-(-log(1-runif(1)*(1-exp(-y[i]*B)))/y[i])
+      y[i+1]<-(-log(1-runif(1)*(1-exp(-x[i+1]*B)))/x[i+1])
+    }
+    x<-x[-(1:burnIn)]
+    y<-y[-(1:burnIn)]
+    return(data.frame(x,y))
   }
-  x<-x[-(1:burnIn)]
-  y<-y[-(1:burnIn)]
-  return(data.frame(x,y))
+ else
+   stop("Initial values incorrect")
 }
